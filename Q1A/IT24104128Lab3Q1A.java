238f9e5df51b7d8d5d10c0a2016263ef86fcee0c
IT24104128 <IT24104128@my.sliit.lk>
2024-11-04 22:54:29 +0530

    Add files via upload

--- Q1A/IT24104128Lab3Q1A.java
+++ Q1A/IT24104128Lab3Q1A.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 
 public class IT24104128Lab3Q1A {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
 
-        // Prompt user to enter the price of 1 kg of rice
-        System.out.print("Enter the price of 1 kg of rice: ");
-        double pricePerKg = scanner.nextDouble();
 
-        // Prompt user to enter the number of kilograms they want to buy
-        System.out.print("Enter the number of kilograms you want to buy: ");
-        double kilograms = scanner.nextDouble();
+System.out.print("Enter the price of 1 kg of rice: ");
+double pricePerKg = scanner.nextDouble();
 
-        // Calculate the total amount to pay
-        double totalAmount = pricePerKg * kilograms;
-
-        // Display the total amount
-        System.out.println("The total amount you have to pay is: " + totalAmount);
-    }
+System.out.print("Enter the number of kilograms you want to buy: ");
+double kilograms = scanner.nextDouble();
+double totalAmount = pricePerKg * kilograms;
+System.out.println("The total amount you have to pay is: " + totalAmount);
+}
 }
